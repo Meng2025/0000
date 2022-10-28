19d2ee66c8198f3a154889c2cfbffca0edd9e46b
Meng@win <565000561@qq.com>
2022-10-29 01:07:59 +0800

    能动

--- applications/mecanum.c
+++ applications/mecanum.c
@@ -3,12 +3,30 @@
 //#include "sbus.h"
 #include "dbus.h"
 
+struct rt_device_pwm *pwm_dev;      /* PWM设备句柄 */
+
 /*----------------------   pwm 周期占空比初始化配置   --------------------------*/
 int mecanum_pwm_init(void)
 {
-    rt_err_t result = RT_EOK;
+    rt_pin_mode(MECANUM_LF_A, PIN_MODE_OUTPUT);
+    rt_pin_mode(MECANUM_LF_B, PIN_MODE_OUTPUT);
+    rt_pin_mode(MECANUM_LB_A, PIN_MODE_OUTPUT);
+    rt_pin_mode(MECANUM_LB_B, PIN_MODE_OUTPUT);
+    rt_pin_mode(MECANUM_RB_A, PIN_MODE_OUTPUT);
+    rt_pin_mode(MECANUM_RB_B, PIN_MODE_OUTPUT);
+    rt_pin_mode(MECANUM_RF_A, PIN_MODE_OUTPUT);
+    rt_pin_mode(MECANUM_RF_B, PIN_MODE_OUTPUT);
     
-    struct rt_device_pwm *pwm_dev;
+    rt_pin_write(MECANUM_LF_A, PIN_HIGH);
+    rt_pin_write(MECANUM_LF_B, PIN_HIGH);
+    rt_pin_write(MECANUM_LB_A, PIN_HIGH);
+    rt_pin_write(MECANUM_LB_B, PIN_HIGH);
+    rt_pin_write(MECANUM_RB_A, PIN_HIGH);
+    rt_pin_write(MECANUM_RB_B, PIN_HIGH);
+    rt_pin_write(MECANUM_RF_A, PIN_HIGH);
+    rt_pin_write(MECANUM_RF_B, PIN_HIGH);    
+    
+    rt_err_t result = RT_EOK;
     
     pwm_dev = (struct rt_device_pwm *)rt_device_find(MECANUM_PWM_DEV);
     
@@ -33,6 +51,116 @@ int mecanum_pwm_init(void)
 INIT_APP_EXPORT(mecanum_pwm_init);
 
 
+/* 解算方法 */
+
+static void motor_pwm_set1(rt_int16_t _p)
+{
+    rt_int32_t __p = 0;
+    __p = _p;
+    
+    if (_p >= 0)
+    {
+        rt_pin_write(MECANUM_LF_A, PIN_HIGH);
+        rt_pin_write(MECANUM_LF_B, PIN_LOW);
+    }
+    else
+    {
+        rt_pin_write(MECANUM_LF_A, PIN_LOW);
+        rt_pin_write(MECANUM_LF_B, PIN_HIGH);
+        __p = -__p;
+    }
+ 
+    __p = 10*__p;
+    
+    rt_pwm_set(pwm_dev, MECANUM_PWM_LF, 10000, __p);
+}
+
+
+static void motor_pwm_set2(rt_int16_t _p)
+{
+    rt_int32_t __p = 0;
+    __p = _p;
+    
+    if (_p >= 0)
+    {
+        rt_pin_write(MECANUM_LB_A, PIN_HIGH);
+        rt_pin_write(MECANUM_LB_B, PIN_LOW);
+    }
+    else
+    {
+        rt_pin_write(MECANUM_LB_A, PIN_LOW);
+        rt_pin_write(MECANUM_LB_B, PIN_HIGH);
+        __p = -__p;
+    }
+ 
+    __p = 10*__p;
+    
+    rt_pwm_set(pwm_dev, MECANUM_PWM_LB, 10000, __p);
+}
+
+static void motor_pwm_set3(rt_int16_t _p)
+{
+    rt_int32_t __p = 0;
+    __p = _p;
+    
+    if (_p >= 0)
+    {
+        rt_pin_write(MECANUM_RB_A, PIN_HIGH);
+        rt_pin_write(MECANUM_RB_B, PIN_LOW);
+    }
+    else
+    {
+        rt_pin_write(MECANUM_RB_A, PIN_LOW);
+        rt_pin_write(MECANUM_RB_B, PIN_HIGH);
+        __p = -__p;
+    }
+ 
+    __p = 10*__p;
+    
+    rt_pwm_set(pwm_dev, MECANUM_PWM_RB, 10000, __p);
+}
+
+static void motor_pwm_set4(rt_int16_t _p)
+{
+    rt_int32_t __p = 0;
+    __p = _p;
+    
+    if (_p >= 0)
+    {
+        rt_pin_write(MECANUM_RF_A, PIN_HIGH);
+        rt_pin_write(MECANUM_RF_B, PIN_LOW);
+    }
+    else
+    {
+        rt_pin_write(MECANUM_RF_A, PIN_LOW);
+        rt_pin_write(MECANUM_RF_B, PIN_HIGH);
+        __p = -__p;
+    }
+ 
+    __p = 10*__p;
+    
+    rt_pwm_set(pwm_dev, MECANUM_PWM_RF, 10000, __p);
+}
+
+
+static void chassis_control(rt_int16_t _x, rt_int16_t _y, rt_int16_t _r)
+{
+    rt_int16_t _lf = 0;
+    rt_int16_t _lb = 0;    
+    rt_int16_t _rb = 0;
+    rt_int16_t _rf = 0;
+    
+    _lf = _x - _y - _r;
+    _lb = _x + _y - _r;
+    _rb = _x + _y + _r;
+    _rf = _x - _y + _r;
+        
+	motor_pwm_set1(_lf);
+	motor_pwm_set2(_lb);
+	motor_pwm_set3(_rb);
+	motor_pwm_set4(_rf);
+}
+
 
 /*----------------------   底盘控制控制线程   --------------------------*/
 
@@ -41,36 +169,10 @@ static rt_thread_t mecanum_pwm_thread = RT_NULL;
 
 static void mecanum_pwm_thread_entry(void *parameter)
 {
-    /* x,横向，y纵向，r旋转 控制量 */
-    rt_int32_t x, y, r;
-    
-    /* 麦轮速度解算 */
-    rt_int32_t pwm_lf;
-    rt_int32_t pwm_lb;
-    rt_int32_t pwm_rb;
-    rt_int32_t pwm_rf;
-    
-    struct rt_device_pwm *pwm_dev;      /* PWM设备句柄 */
-    pwm_dev = (struct rt_device_pwm *)rt_device_find(MECANUM_PWM_DEV);
-    
-    rt_pin_mode(MECANUM_LF_A, PIN_MODE_OUTPUT);
-    rt_pin_mode(MECANUM_LF_B, PIN_MODE_OUTPUT);
-    rt_pin_mode(MECANUM_LB_A, PIN_MODE_OUTPUT);
-    rt_pin_mode(MECANUM_LB_B, PIN_MODE_OUTPUT);
-    rt_pin_mode(MECANUM_RB_A, PIN_MODE_OUTPUT);
-    rt_pin_mode(MECANUM_RB_B, PIN_MODE_OUTPUT);
-    rt_pin_mode(MECANUM_RF_A, PIN_MODE_OUTPUT);
-    rt_pin_mode(MECANUM_RF_B, PIN_MODE_OUTPUT);
-    
-    rt_pin_write(MECANUM_LF_A, PIN_HIGH);
-    rt_pin_write(MECANUM_LF_B, PIN_HIGH);
-    rt_pin_write(MECANUM_LB_A, PIN_HIGH);
-    rt_pin_write(MECANUM_LB_B, PIN_HIGH);
-    rt_pin_write(MECANUM_RB_A, PIN_HIGH);
-    rt_pin_write(MECANUM_RB_B, PIN_HIGH);
-    rt_pin_write(MECANUM_RF_A, PIN_HIGH);
-    rt_pin_write(MECANUM_RF_B, PIN_HIGH);
-    
+    rt_int16_t x = 0;
+    rt_int16_t y = 0;
+    rt_int16_t r = 0;
+
     while (1)
     {
 
@@ -101,6 +203,7 @@ static void mecanum_pwm_thread_entry(void *parameter)
 
 /* 大疆 遥控器 */
 #ifdef  __DBUS_H__
+
         x = dbus.lh;
         y = dbus.lv;
         r = dbus.rh;
@@ -109,73 +212,9 @@ static void mecanum_pwm_thread_entry(void *parameter)
         y = (y - 1024) * 500 / 660;
         r = (r - 1024) * 500 / 660;       
 #endif
-        
-        /* xyr 速度到4轮速度映射 */
-        pwm_lf = y + x - r;     /* A */     pwm_rf = y - x + r;     /* B */
-        pwm_lb = y - x - r;     /* B */     pwm_rb = y + x + r;     /* A */
-        
-//        rt_kprintf("A:%4d B:%4d B:%4d A:%4d\n", pwm_lf, pwm_lb, pwm_rf ,pwm_rb);
 
-        /* --------------------------------------------------------------------------------------------------------------- */
-        if (pwm_lf>0)                                      
-        {
-            rt_pin_write(MECANUM_LF_A, PIN_HIGH);                   
-            rt_pin_write(MECANUM_LF_B, PIN_LOW);
-        }
-        else
-        {
-            pwm_lf = -pwm_lf;
-            rt_pin_write(MECANUM_LF_A, PIN_LOW);
-            rt_pin_write(MECANUM_LF_B, PIN_HIGH);
-        }
-                                                                    if (pwm_rf>0)
-                                                                    {
-                                                                        rt_pin_write(MECANUM_RF_A, PIN_LOW);
-                                                                        rt_pin_write(MECANUM_RF_B, PIN_HIGH);
-                                                                    }
-                                                                    else
-                                                                    {
-                                                                        pwm_rf = -pwm_rf;
-                                                                        rt_pin_write(MECANUM_RF_A, PIN_HIGH);
-                                                                        rt_pin_write(MECANUM_RF_B, PIN_LOW);   
-                                                                    }
-                                                                    
-        /* --------------------------------------------------------------------------------------------------------------- */
-        if (pwm_lb>0)
-        {
-            rt_pin_write(MECANUM_LB_A, PIN_HIGH);
-            rt_pin_write(MECANUM_LB_B, PIN_LOW);
-        }
-        else
-        {
-            pwm_lb = -pwm_lb;
-            rt_pin_write(MECANUM_LB_A, PIN_LOW);
-            rt_pin_write(MECANUM_LB_B, PIN_HIGH);
-        }        
-                                                                     if (pwm_rb>0)
-                                                                     {
-                                                                         rt_pin_write(MECANUM_RB_A, PIN_LOW);
-                                                                         rt_pin_write(MECANUM_RB_B, PIN_HIGH);
-                                                                     }
-                                                                     else
-                                                                     {
-                                                                         pwm_rb = -pwm_rb;
-                                                                         rt_pin_write(MECANUM_RB_A, PIN_HIGH);
-                                                                         rt_pin_write(MECANUM_RB_B, PIN_LOW);   
-                                                                     }        
-        /* --------------------------------------------------------------------------------------------------------------- */
-        
 
-        /* pwm信号输出 */
-        pwm_lf = pwm_lf * 10;
-        pwm_lb = pwm_lb * 10;
-        pwm_rb = pwm_rb * 10;
-        pwm_rf = pwm_rf * 10;
-
-        rt_pwm_set(pwm_dev, MECANUM_PWM_LF, 10000, pwm_lf);         rt_pwm_set(pwm_dev, MECANUM_PWM_RF, 10000, pwm_rf);
-        rt_pwm_set(pwm_dev, MECANUM_PWM_LB, 10000, pwm_lb);         rt_pwm_set(pwm_dev, MECANUM_PWM_RB, 10000, pwm_rb);
-        
-         
+        chassis_control(x, y, r);
 
         rt_thread_mdelay(10);
     }
